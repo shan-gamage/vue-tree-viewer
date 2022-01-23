ca0dd2305ee4b49ac742f50da23e084da20d05a8
Shanaka Gamage <shanakanuwan10@gmail.com>
2022-01-23 15:02:35 +0800

    Implemented to generate tree according to the provided tree Json

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,80 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <VueTreeViewer msg="Welcome to Your Vue.js App"/>
-  </div>
+	<div id="app">
+		<img alt="Vue logo" src="./assets/logo.png">
+		<VueTreeViewer :nodes="list" />
+	</div>
 </template>
 
 <script>
 import VueTreeViewer from './components/VueTreeViewer.vue'
 
 export default {
-  name: 'App',
-  components: {
-    VueTreeViewer
+	name:'App',
+	components: {
+		VueTreeViewer
+	},
+	data() {
+		return {
+			list : [
+  {
+    "name": "parent",
+    "nodeUUID": "92360bad-c6f9-403a-92e6-320dcafe8a8b",
+    "children": [
+      {
+        "name": "test1",
+        "nodeUUID": "461b3778-7c16-11ec-90d6-0242ac120003",
+        "children": [
+          {
+            "name": "test2",
+            "nodeUUID": "461b39bc-7c16-11ec-90d6-0242ac120003"
+          },
+          {
+            "name": "test3",
+            "nodeUUID": "461b3af2-7c16-11ec-90d6-0242ac120003",
+            "children": [
+              {
+                "name": "test5",
+                "nodeUUID": "461b3c14-7c16-11ec-90d6-0242ac120003"
+              }
+            ]
+          },
+          {
+            "name": "test3",
+            "nodeUUID": "461b3d22-7c16-11ec-90d6-0242ac120003"
+          },
+          {
+            "name": "test4",
+            "nodeUUID": "461b3fde-7c16-11ec-90d6-0242ac120003",
+            "children": [
+              {
+                "name": "test6",
+                "nodeUUID": "461b4100-7c16-11ec-90d6-0242ac120003",
+                "children": [
+                  {
+                    "name": "test7",
+                    "nodeUUID": "461b4254-7c16-11ec-90d6-0242ac120003"
+                  }
+                ]
+              }
+            ]
+          }
+        ]
+      }
+    ]
   }
+]
+		}
+	}
 }
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+	font-family: Avenir, Helvetica, Arial, sans-serif;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+	text-align: center;
+	color: #2c3e50;
+	margin-top: 60px;
 }
 </style>
--- src/components/VueTreeViewer.vue
+++ src/components/VueTreeViewer.vue
@@ -1,58 +1,65 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
-  </div>
+    <div>
+        <div
+            v-for="node in sortedNodes"
+            :key="node.nodeUUID"
+            :style="{ 'margin-left': `${depth * 30}px` }"
+            class="node"
+        >
+            {{ node.name }}
+            <div :id="'tree-' + node.nodeUUID">
+                <VueTreeViewer
+                    v-if="node.children"
+                    :nodes="node.children"
+                    :depth="1"
+                />
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
 export default {
-  name: 'VueTreeViewer',
-  props: {
-    msg: String
-  }
+	name: 'VueTreeViewer',
+	props: {
+		nodes: Array,
+        depth: {
+            type: Number,
+            default: 0,
+        },
+	},
+	data() {
+		return {
+        expanded: this.expandable,
+        selectedNode: Object,
+        sortedNodes: [],
+        doSort: false
+		};
+	},
+	created() {
+        if(this.doSort) {
+            this.nodes.map((child) => {
+                this.sortedNodes.push(this.sortTree(child));
+            });
+        } else {
+            this.sortedNodes = this.nodes;   
+        }
+	},
+	methods: {
+		sortTree(node) {
+            if (node.children && node.children.length > 0) {
+                node.children.sort((a, b) => {
+                    if (!a.children || a.children.length === 0 && b.children && b.children.length > 0) {
+                        return -1;
+                    } else if (a.children && a.children.length > 0 && !b.children || b.children.length === 0) {
+                        return 1;
+                    } else {
+                        return;
+                    }
+                });
+            }
+            return node;
+        }
+	}
 }
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
